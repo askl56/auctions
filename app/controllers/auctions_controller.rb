72950ebe694c45cfbd8139954bf4f5dbc2197dde
askl56 <amscott@openmailbox.org>
2015-04-20 02:33:46 +0100

    begun to add functionality

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -2,7 +2,7 @@ class AuctionsController < ApplicationController
 	def create
 		product = Product.find params[:product_id]
 		auction = Auction.new auction_params.merge! product_id: product.id
-
+	
 		if auction.save
 			redirect_to product, notice: "Product was put to auction."
 		else
